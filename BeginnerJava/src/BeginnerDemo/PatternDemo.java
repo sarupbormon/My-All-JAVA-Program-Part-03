b9d7cb38a21fdf856590ea890162ad9b974fe50d
Sarup <sarupbormon18@gmail.com>
2024-10-22 19:22:31 +0530

    Program-47

---
+++ BeginnerJava/src/BeginnerDemo/PatternDemo.java
@@ -0,0 +1,20 @@
+package BeginnerDemo;
+import java.util.Scanner;
+public class PatternDemo {
+    public static void main(String[] args) {
+    	
+    	Scanner input = new Scanner(System.in);
+    	System.out.println("Please Enter Line Number: ");
+    	int n = input.nextInt();
+    	
+    	for(int row = 1; row <= n; row++) {
+    		for(int col = 1; col <= row; col++) {
+    			
+    			System.out.print(" "+col);
+    		}
+    		System.out.println();
+    	}
+    			
+		
+	}
+}
